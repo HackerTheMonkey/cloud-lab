c509c04d43c3730eac6ad29de2c0a1f26eecd86a
HackerMonkey <joseph.khafaji@gmail.com>
2020-06-28 15:40:31 +0100

    adjusting the requirements

--- main.go
+++ main.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+)
+
+var url = "https://httpbin.org/get"
 
 func main() {
-	fmt.Println("code does matter, indeed!")
+	// send a request
+	response, error := http.Get(url)
+	if error != nil {
+		fmt.Fprintf(os.Stderr, "fetch: %v\n", error)
+	}
+
+	// parse the body
+	bytes, error := ioutil.ReadAll(response.Body)
+	if error != nil {
+		fmt.Fprintf(os.Stderr, "parsing HTTP response body: %v\n", error)
+		os.Exit(1)
+	}
+	response.Body.Close()
+
+	// print the body out
+	fmt.Printf("%s\n", bytes)
 }
